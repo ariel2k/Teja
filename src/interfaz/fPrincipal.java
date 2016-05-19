b8a2527e018ef2673487b6c730d26371b86c8f52
Ariel Molina <arielhmolina@gmail.com>
2016-05-18 21:34:11 -0300

    interfaz lista archivos de directorio

--- src/interfaz/fPrincipal.java
+++ src/interfaz/fPrincipal.java
@@ -6,10 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.ListModel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
 import javax.swing.JList;
@@ -20,11 +23,36 @@ import javax.swing.JToggleButton;
 import java.awt.SystemColor;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JFileChooser;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileFilter;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 
 public class fPrincipal extends JFrame {
 
 	private JPanel contentPane;
 	private JLabel lbl1;
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu mnArchivo = new JMenu("Archivo");
+	private JMenuItem mntmAbrirCarpeta = new JMenuItem("Abrir carpeta");
+	private JFileChooser fc = new JFileChooser();
+	private JList lClases = new JList();
+	private JList lMetodos = new JList();
+	private JList lArchivos = new JList();
+	private JLabel lblSeleccionarArchivoA = new JLabel("Seleccionar archivo:");
+	private JLabel lblElegirClase = new JLabel("Elegir clase:");
+	private JLabel lblElegirMtodo = new JLabel("Elegir m\u00E9todo:");
+	private JLabel lblCdigoMtodo = new JLabel("C\u00F3digo del m\u00E9todo:");
+	private JTextArea txtaCodigo = new JTextArea();
+	private JPanel panel = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -51,13 +79,17 @@ public class fPrincipal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 727, 611);
 		
-		JMenuBar menuBar = new JMenuBar();
+		
 		setJMenuBar(menuBar);
 		
-		JMenu mnArchivo = new JMenu("Archivo");
-		menuBar.add(mnArchivo);
 		
-		JMenuItem mntmAbrirCarpeta = new JMenuItem("Abrir carpeta");
+		menuBar.add(mnArchivo);
+		mntmAbrirCarpeta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				listarArchivos();
+			}
+		});
+		
 		mntmAbrirCarpeta.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
 		mnArchivo.add(mntmAbrirCarpeta);
 		
@@ -69,40 +101,40 @@ public class fPrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList lClases = new JList();
+		
 		lClases.setBounds(10, 145, 133, 165);
 		contentPane.add(lClases);
 		
-		JList lMetodos = new JList();
+		
 		lMetodos.setBounds(153, 145, 311, 165);
 		contentPane.add(lMetodos);
 		
-		JList lArchivos = new JList();
+		
 		lArchivos.setBounds(10, 32, 454, 77);
 		contentPane.add(lArchivos);
 		
-		JLabel lblSeleccionarArchivoA = new JLabel("Seleccionar archivo:");
+		
 		lblSeleccionarArchivoA.setBounds(10, 11, 168, 14);
 		contentPane.add(lblSeleccionarArchivoA);
 		
-		JLabel lblElegirClase = new JLabel("Elegir clase:");
+		
 		lblElegirClase.setBounds(10, 125, 133, 14);
 		contentPane.add(lblElegirClase);
 		
-		JLabel lblElegirMtodo = new JLabel("Elegir m\u00E9todo:");
+		
 		lblElegirMtodo.setBounds(153, 125, 133, 14);
 		contentPane.add(lblElegirMtodo);
 		
-		JLabel lblCdigoMtodo = new JLabel("C\u00F3digo del m\u00E9todo:");
+		
 		lblCdigoMtodo.setBounds(10, 321, 133, 14);
 		contentPane.add(lblCdigoMtodo);
 		
-		JTextArea txtaCodigo = new JTextArea();
+		
 		txtaCodigo.setEditable(false);
 		txtaCodigo.setBounds(10, 339, 701, 211);
 		contentPane.add(txtaCodigo);
 		
-		JPanel panel = new JPanel();
+		
 		panel.setBackground(SystemColor.text);
 		panel.setBounds(475, 32, 236, 278);
 		contentPane.add(panel);
@@ -140,56 +172,56 @@ public class fPrincipal extends JFrame {
 		lbl8.setBounds(10, 245, 136, 22);
 		panel.add(lbl8);
 		
-		JLabel lblLCodTotales = new JLabel("999999");
+		JLabel lblLCodTotales = new JLabel("");
 		lblLCodTotales.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblLCodTotales.setForeground(SystemColor.activeCaption);
 		lblLCodTotales.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblLCodTotales.setBounds(145, 7, 81, 28);
 		panel.add(lblLCodTotales);
 		
-		JLabel lblLComentarios = new JLabel("999999");
+		JLabel lblLComentarios = new JLabel("");
 		lblLComentarios.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblLComentarios.setForeground(SystemColor.activeCaption);
 		lblLComentarios.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblLComentarios.setBounds(145, 44, 81, 28);
 		panel.add(lblLComentarios);
 		
-		JLabel lblPComentarios = new JLabel("999999");
+		JLabel lblPComentarios = new JLabel("");
 		lblPComentarios.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblPComentarios.setForeground(SystemColor.activeCaption);
 		lblPComentarios.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblPComentarios.setBounds(145, 76, 81, 28);
 		panel.add(lblPComentarios);
 		
-		JLabel lblComplejidadCiclomatica = new JLabel("999999");
+		JLabel lblComplejidadCiclomatica = new JLabel("");
 		lblComplejidadCiclomatica.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblComplejidadCiclomatica.setForeground(SystemColor.activeCaption);
 		lblComplejidadCiclomatica.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblComplejidadCiclomatica.setBounds(145, 109, 81, 28);
 		panel.add(lblComplejidadCiclomatica);
 		
-		JLabel lblFanIn = new JLabel("999999");
+		JLabel lblFanIn = new JLabel("");
 		lblFanIn.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblFanIn.setForeground(SystemColor.activeCaption);
 		lblFanIn.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblFanIn.setBounds(145, 142, 81, 28);
 		panel.add(lblFanIn);
 		
-		JLabel lblFanOut = new JLabel("999999");
+		JLabel lblFanOut = new JLabel("");
 		lblFanOut.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblFanOut.setForeground(SystemColor.activeCaption);
 		lblFanOut.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblFanOut.setBounds(145, 175, 81, 28);
 		panel.add(lblFanOut);
 		
-		JLabel lblHalsteadLongitud = new JLabel("999999");
+		JLabel lblHalsteadLongitud = new JLabel("");
 		lblHalsteadLongitud.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblHalsteadLongitud.setForeground(SystemColor.activeCaption);
 		lblHalsteadLongitud.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblHalsteadLongitud.setBounds(145, 208, 81, 28);
 		panel.add(lblHalsteadLongitud);
 		
-		JLabel lblHalsteadVolumen = new JLabel("999999");
+		JLabel lblHalsteadVolumen = new JLabel("");
 		lblHalsteadVolumen.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblHalsteadVolumen.setForeground(SystemColor.activeCaption);
 		lblHalsteadVolumen.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -199,5 +231,20 @@ public class fPrincipal extends JFrame {
 		JLabel lblMtricasDelMtodo = new JLabel("M\u00E9tricas del m\u00E9todo:");
 		lblMtricasDelMtodo.setBounds(475, 11, 168, 14);
 		contentPane.add(lblMtricasDelMtodo);
+	}
+	
+	private void listarArchivos(){
+		JFileChooser chooser = new JFileChooser();
+	    chooser.setCurrentDirectory(new java.io.File("."));
+	    chooser.setDialogTitle("choosertitle");
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    chooser.setAcceptAllFileFilterUsed(false);
+
+	    if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+	      File dir = new File(chooser.getSelectedFile() + "");
+	      String[] ficheros = dir.list();
+	      if (ficheros != null)
+	    	  this.lArchivos.setListData(ficheros);
+	    } 
 	}
 }
